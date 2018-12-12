f1f6e33b9f9cf6f67e0eb255869d6f32c8ad5b2d
sw <genie_s2@naver.com>
2018-12-12 09:04:21 +0900

    end

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./styles.scss";
+
+export default () => (
+    <div className={styles.title}>
+        dddddd<h1>Hello!</h1>
+        <p>test</p>
+    </div>
+);
